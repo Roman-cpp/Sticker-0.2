a42e0ae48d8d45c81002b39d353f8d13b07bcfe9
Roman-cpp <85461255+Roman-cpp@users.noreply.github.com>
2022-03-27 07:38:28 +0300

    Добавьте файлы проекта.

---
+++ Sticker 0.2/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Threading;
+
+
+namespace Sticker_0._2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            //copy from fail.txt
+            using (FileStream fstream = new FileStream($"{path}\\TextFile.txt", FileMode.OpenOrCreate))
+            {
+                // преобразуем строку в байты
+                byte[] array = new byte[fstream.Length];
+                // считываем данные
+                fstream.Read(array, 0, array.Length);
+                // декодируем байты в строку
+                string textFromFile = System.Text.Encoding.Default.GetString(array);
+                text.Text = textFromFile;
+            }
+
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, Properties.Settings.Default.minut);
+            dispatcherTimer.Start();
+        }
+
+        //Context Menu
+        private void pasteMenuItem_Click(object sender, EventArgs e) => text.Paste();
+        private void copyMenuItem_Click(object sender, EventArgs e) => text.Copy();
+        private void cutMenuItem_Click(object sender, EventArgs e) => text.Cut();
+        private void exitMenuItem_Click(object sender, EventArgs e) => Application.Current.Shutdown();
+        private void settingMenuItem_Ckick(object sender, EventArgs e)
+        {
+            settings settings = new settings();
+            settings.ShowDialog();
+        }
+
+
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            using (FileStream fstream = new FileStream($"{path}\\TextFile.txt", FileMode.Create))
+            {
+                // преобразуем строку в байты
+                byte[] array = System.Text.Encoding.Default.GetBytes(text.Text);
+                // запись массива байтов в файл
+                fstream.Write(array, 0, array.Length);
+                //Console.WriteLine("Текст записан в файл");
+            }
+        }
+
+
+        private void movingMainWindow_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (!pin.IsChecked == true)
+            {
+                DragMove();
+            }
+
+        }
+        
+        private void pin_LostMouseCapture(object sender, MouseEventArgs e)
+        {
+            Properties.Settings.Default.X = Left.ToString();
+            Properties.Settings.Default.Y = Top.ToString();
+            Properties.Settings.Default.Save();
+        }
+
+        private string path = Directory.GetCurrentDirectory();
+        private DispatcherTimer dispatcherTimer = new DispatcherTimer(); 
+    }
+}
---
+++ Sticker 0.2/settings.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Drawing;
+using FontText = System.Windows.Media.FontFamily;
+
+namespace Sticker_0._2
+{
+    /// <summary>
+    /// Логика взаимодействия для settings.xaml
+    /// </summary>
+    public partial class settings : Window
+    {
+        public settings()
+        {
+            InitializeComponent();
+            //font
+            foreach (FontText font_family in Fonts.SystemFontFamilies)
+                font_box.Items.Add(font_family.Source);
+
+
+            _minut = Convert.ToInt32(Properties.Settings.Default.minut);
+            _list_color = Properties.Settings.Default.list_color;
+            _font_color = Properties.Settings.Default.font_color;
+            _font_size = Properties.Settings.Default.font_size;
+            _font = Properties.Settings.Default.font;
+        }
+
+
+        private void colorFont_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = e.Source as TextBox;
+            if (textBox.Text.Length == 7)
+                Properties.Settings.Default.font_color = textBox.Text;
+        }
+
+        private void colorList_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = e.Source as TextBox;
+            if (textBox.Text.Length == 7)
+                Properties.Settings.Default.list_color = textBox.Text;
+        }
+
+
+        private void ok_Click(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.minut = Convert.ToInt32(minut.Text);
+            Properties.Settings.Default.list_color = color_list.Text;
+            Properties.Settings.Default.font_color = color_font.Text;
+            Properties.Settings.Default.font_size = text_size.Text;
+            Properties.Settings.Default.font = font_box.Text;
+            Properties.Settings.Default.Save();
+
+            this.Close();
+        }
+        private void cancel_Click(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.minut = _minut;
+            Properties.Settings.Default.list_color = _list_color;
+            Properties.Settings.Default.font_color = _font_color;
+            Properties.Settings.Default.font_size = _font_size;
+            Properties.Settings.Default.font = _font;
+         
+            this.Close();
+        }
+        private void factorySettings_Click(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.minut = 5;
+            Properties.Settings.Default.list_color = "#fff79a";
+            Properties.Settings.Default.font_color = "#000000";
+            Properties.Settings.Default.font_size = "24";
+            Properties.Settings.Default.font = "PF Scandal Pro Black";
+            Properties.Settings.Default.Save();
+            this.Close();
+        }
+
+        private int _minut;
+        private string _list_color;
+        private string _font_color;
+        private string _font_size;
+        private string _font;
+
+    }
+}
